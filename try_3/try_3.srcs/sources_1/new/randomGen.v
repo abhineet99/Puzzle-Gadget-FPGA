478eed7aaddfeb7d793078022229fa83933ac0f8
Abhineet Pandey <36227377+abhineet99@users.noreply.github.com>
2018-11-18 12:25:30 +0530

    Add files via upload

---
+++ try_3/try_3.srcs/sources_1/new/randomGen.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.11.2018 11:15:35
+// Design Name: 
+// Module Name: randomGen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module randomGen(
+    input [1:0] inBtn,
+    input [3:0] inRand,
+    output reg [3:0]outNum
+    );
+reg [3:0] to_out;
+always@*
+if(inBtn==2'b01)
+begin 
+to_out[3:0]=inRand[3:0];    
+end    
+endmodule
