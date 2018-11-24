6115e2e4429ab4703e37c4b4f23c9ef509c6e692
Abhineet Pandey <36227377+abhineet99@users.noreply.github.com>
2018-11-24 17:52:25 +0530

    Working.

--- main.v
+++ main.v
@@ -30,13 +30,15 @@ module seg7decimal(
     input btnl
 	 );
 	 
-	 
-wire [1:0] s;	 
+reg [3:0] offset;
+wire [1:0] s;	
+reg [3:0] j=0; 
 reg [8:0] whichdone;
 initial begin
 whichdone=9'b00000000;//which matrix slot already has a number
 end
 reg [3:0] digit;
+reg [3:0] to_digit;
 reg [3:0] todisplay;
 wire [3:0] aen;
 reg [19:0] clkdiv;
@@ -44,23 +46,17 @@ reg [3:0] lol;
 reg [11:0] row1;//matrix rows
 reg [11:0] row2;
 reg [11:0] row3;
-reg [1:0]btnonce;
+reg btnonce=0;
 //assign btnonce=2'b01;
-initial begin
-btnonce=2'b00;
-lol=4'b0001; //number to be entered in the matrix
-row1=12'b000000000000;
-row2=12'b000000000000;
-row3=12'b000000000000;
-end
+
 
 always @(posedge clk)// or posedge clr)
-if(btnl==1)
+if(btnl==1 && btnonce!=1)
 begin
    
    digit=clkdiv[3:0];
-   digit=digit%9;
-   
+   digit=digit%7;
+   btnonce=1'b1;
   // if(whichdone[digit]==0)
    //begin
    whichdone[digit]=1;
@@ -79,21 +75,78 @@ begin
    endcase
    lol=lol+1;
    lol=lol%9;
-   
+   //btnonce=1;
    //end
    
 end    
-   
+else if (btnonce==1)
+begin
+j=j+1;
+offset=5-digit%5;
+to_digit=((digit+j*offset)%7);
+
+
+ if(whichdone[to_digit]==0)
+begin
+j=0;
+whichdone[to_digit]=1;
+case (to_digit)
+0:row1[11:8]=lol[3:0];
+1:row1[7:4]=lol[3:0];
+2:row1[3:0]=lol[3:0];
+3:row2[11:8]=lol[3:0];
+4:row2[7:4]=lol[3:0];
+5:row2[3:0]=lol[3:0];
+6:row3[11:8]=lol[3:0];
+7:row3[7:4]=lol[3:0];
+8:row3[3:0]=lol[3:0];
+
+
+endcase
+digit=to_digit;
+lol=lol+1;
+lol=lol%9;
+end
+//else
+//begin
+//digit=digit+1;
+//digit=digit%9;
+//end
+end 
+/* 
+else if(lol==8)
+begin
+
+if(whichdone[7]==0)
+begin 
+row3[7:4]=lol[3:0];
+lol=lol+1;
+
+end
+else if(whichdone[8]==0)
+begin 
+row3[3:0]=lol[3:0];
+lol=lol+1;
+
+end
+*/
+initial begin
+//btnonce=1'b0;
+lol=4'b0001; //number to be entered in the matrix
+row1=12'b000000000000;
+row2=12'b000000000000;
+row3=12'b000010000000;
+end
    
 assign dp = 0;
 assign s = clkdiv[19:18];
 assign aen = 4'b1111; // all turned off initially
-
+/*
 // quad 4to1 MUX.
 always @(posedge clk)
 if(btnonce==1)
 begin
-digit=(digit+clkdiv[7:4])^clkdiv[11:8]^clkdiv[10:7];
+digit=((digit*7)%17)%9;
 digit=digit%9;
 
  if(whichdone[digit]==0)
@@ -116,7 +169,7 @@ lol=lol+1;
 lol=lol%9;
 end
 end
-
+*/
 always @(posedge clk)// or posedge clr)
 if(x[0]==1)
 begin	   
@@ -180,7 +233,7 @@ case(todisplay)
 7:a_to_g = 7'b1111000;////0111
 8:a_to_g = 7'b0000000;////1000
 9:a_to_g = 7'b0010000;////1001
-10: a_to_g=7'b11111111;
+10: a_to_g=7'b1111111;
 'hA:a_to_g = 7'b0111111; // dash-(g)
 'hB:a_to_g = 7'b1111111; // all turned off
 'hC:a_to_g = 7'b1110111;
